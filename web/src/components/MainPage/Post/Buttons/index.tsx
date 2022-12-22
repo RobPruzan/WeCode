eb426b06ce857b56ba75ab15edcc23e13275a776
RobPruzan <rapruzan@buffalo.edu>
2022-12-21 21:12:46 -0500

    Tabs with icons

---
+++ web/src/components/MainPage/Post/Buttons/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+const Buttons = () => {
+  return (
+    <>
+      <Card>Buttons</Card>
+    </>
+  );
+};
+
+export default Buttons;
