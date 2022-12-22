f232742a50ce1632cd208151afc482454876f4ee
RobPruzan <rapruzan@buffalo.edu>
2022-12-22 00:08:15 -0500

    Styling of site

--- web/src/components/MainPage/Post/Tabs/TextTab/index.tsx
+++ web/src/components/MainPage/Post/Tabs/TextTab/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { SendPost } from '../../Buttons/SendPost';
 import { InputText } from './InputText';
 
 export const TextTab = () => {
   return (
     <>
       <InputText />
+      <SendPost />
     </>
   );
 };
