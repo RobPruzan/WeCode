fd5351e02e95c161b8027c22edf0b834b7b17a6e
RobPruzan <rapruzan@buffalo.edu>
2022-12-26 16:20:37 -0500

    Add upvote limits and color changes

--- web/src/components/MainPage/FeedView/PostedContents/UpDownVoting.tsx
+++ web/src/components/MainPage/FeedView/PostedContents/UpDownVoting.tsx
@@ -2,25 +2,49 @@ import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import { Dispatch, SetStateAction, useState } from 'react';
 export type UpDownVotingProps = {
-  setUpVotes: Dispatch<SetStateAction<number>>;
   upVotes: number;
+  setUpVotes: Dispatch<SetStateAction<number>>;
+  disabled?: boolean;
+  handleUpVote: VoidFunction;
+  handleDownVote: VoidFunction;
+  upOrDownVote: 'up' | 'down' | null;
 };
-export const UpDownVoting = ({ upVotes, setUpVotes }: UpDownVotingProps) => {
+
+export const UpDownVoting = ({
+  upVotes,
+  setUpVotes,
+  disabled,
+  handleUpVote,
+  handleDownVote,
+  upOrDownVote,
+}: UpDownVotingProps) => {
+  const getColor = (direction: 'up' | 'down') => {
+    if (disabled) {
+      if (upOrDownVote === direction) {
+        return 'white';
+      } else {
+        return 'grey';
+      }
+    } else {
+      return 'grey';
+    }
+  };
   return (
     <>
       <p className="h4">{upVotes}</p>
-      <div
-        style={{ cursor: 'pointer' }}
-        onClick={() => setUpVotes(prev => prev + 1)}
-      >
-        <ThumbUpIcon color="primary" />
+      <div style={{ cursor: 'pointer' }} onClick={handleUpVote}>
+        <ThumbUpIcon
+          style={{
+            fill: getColor('up'),
+          }}
+        />
       </div>
-      <div
-        style={{ cursor: 'pointer' }}
-        onClick={() => setUpVotes(prev => prev - 1)}
-      >
-        {' '}
-        <ThumbDownIcon color="primary" />
+      <div style={{ cursor: 'pointer' }} onClick={handleDownVote}>
+        <ThumbDownIcon
+          style={{
+            fill: getColor('down'),
+          }}
+        />
       </div>
     </>
   );
