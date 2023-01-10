c475ef908041d40b65fadda7a94c5d1f5013a677
RobPruzan <rapruzan@buffalo.edu>
2023-01-10 04:24:56 -0500

    Improved styling on quiz page

--- web/src/components/MainPage/Challenges/ChallengeQuiz.tsx
+++ web/src/components/MainPage/Challenges/ChallengeQuiz.tsx
@@ -1,8 +1,14 @@
+import {
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from '@mui/material';
 import React, { useEffect, useState } from 'react';
 
 import { ChallengeInfo } from './ChallengesCol';
 import LinearProgress from '@mui/material/LinearProgress';
-import Radio from '@material-ui/core/Radio';
 import green from '@material-ui/core/colors/green';
 
 export type ChallengeQuizProps = {
@@ -29,7 +35,7 @@ const ChallengeQuiz = ({
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (count < 100) {
+      if (count < 500) {
         setCount(count + 1);
       }
     }, 100);
@@ -39,16 +45,58 @@ const ChallengeQuiz = ({
   console.log(count);
   return (
     <>
-      <LinearProgress variant="determinate" value={count} />
-      <p className="h3">{challenge.title}</p>
-      <p className="h3">{challenge.description}</p>
-      <div className=""></div>
-      <button
-        onClick={handleExitChallenge}
-        className="bg-neon-blue hover:bg-blue-500 rounded-lg shadow-lg text-white p-2 m-2"
+      <LinearProgress variant="determinate" value={count / 5} />
+      <p className="h3 mt-2">{challenge.title}</p>
+      <p
+        style={{
+          overflowWrap: 'anywhere',
+        }}
+        className="border-t-2 border-custom-gray mt-2 p-2"
       >
-        exit
-      </button>
+        {challenge.description}
+      </p>
+      <div className="mr-auto ml-3">
+        <FormControl>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="female"
+            name="radio-buttons-group"
+          >
+            <FormControlLabel
+              className="mt-2"
+              value="female"
+              control={<Radio disableRipple={true} />}
+              label="Female"
+            />
+            <FormControlLabel
+              className="mt-2"
+              value="male"
+              control={<Radio />}
+              label="Male"
+            />
+            <FormControlLabel
+              className="mt-2"
+              value="other"
+              control={<Radio />}
+              label="Other"
+            />
+          </RadioGroup>
+        </FormControl>
+      </div>
+      <div className="flex mt-3">
+        <button
+          onClick={handleExitChallenge}
+          className="bg-red-500 hover:bg-red-700 rounded-lg shadow-lg text-white p-2 w-50 mr-2"
+        >
+          Exit
+        </button>
+        <button
+          onClick={handleExitChallenge}
+          className="bg-neon-blue hover:bg-sky-500 rounded-lg shadow-lg text-white p-2 w-50 "
+        >
+          Submit
+        </button>
+      </div>
     </>
   );
 };
