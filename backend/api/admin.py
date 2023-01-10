a051ff6347a5d19e92fbc0a059d43026c6c02a6d
RobPruzan <rapruzan@buffalo.edu>
2023-01-10 17:24:34 -0500

    WIP

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import User
-from .models import Space
+from .models import User, Space, Challenge
 
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Space)
+admin.site.register(Challenge)
