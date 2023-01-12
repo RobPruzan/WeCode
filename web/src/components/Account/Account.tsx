a762351495183b8f40449a7251bdd1dee8a59105
RobPruzan <rapruzan@buffalo.edu>
2023-01-11 20:15:00 -0500

    Account styling change

--- web/src/components/Account/Account.tsx
+++ web/src/components/Account/Account.tsx
@@ -2,6 +2,7 @@ import React, { ChangeEvent, useState } from 'react';
 import WeCode, { PostContent } from '../../services/connections';
 import { useQuery, useQueryClient } from 'react-query';
 
+import { BsCircleFill } from 'react-icons/bs';
 import { CustomTextField } from '../CustomTextField';
 import { DockLocation } from '../Navbars/IconDock';
 import { MainNavbar } from '../Navbars/MainNavbar';
@@ -92,43 +93,45 @@ const Account = () => {
       .sort((a, b) => (b?.id ?? 0) - (a?.id ?? 0));
 
   return (
-    <div className="custom-black-background flex flex-col h-screen w-screen  max-w-full bg-cust">
+    <div className="custom-black-background flex flex-col h-screen w-screen  max-w-full">
       <MainNavbar height="10" location={DockLocation.ACCOUNT} />
       <div className="flex justify-center items-center flex-grow">
         <div className=" grid  grid-rows-8 min-h-full w-full p-3">
           <div className="row-span-2  ">
-            <div className="grid grid-rows-2 h-full">
-              <div className="row-span-1 border-b-2  ">
-                <div className="grid grid-cols-3 h-full">
-                  <div className="col-span-1 text-center flex items-center justify-center">
-                    Followers
+            <div className="grid  h-full p-8">
+              <div className="row-span-1  ">
+                <div className="grid grid-cols-3 h-full border-x-2 border-y-2 border-neon-blue rounded-lg p-4">
+                  <div className="flex justify-center items-center w-full h-full text-center ">
+                    <div className=" text-2xl col-span-1 text-center flex items-center justify-center">
+                      Followers
+                    </div>
+                    <div className=" text-white font-extrabold text-2xl mx-2 ">
+                      {followers?.length ?? 0}
+                    </div>
                   </div>
-                  <div className="col-span-1 text-center flex items-center justify-center">
-                    Following
+                  <div className="flex justify-center items-center w-full h-full text-center ">
+                    <div className=" text-2xl col-span-1 text-center flex items-center justify-center">
+                      Following
+                    </div>
+                    <div className=" text-white font-extrabold text-2xl mx-2 ">
+                      {following?.length ?? 0}
+                    </div>
                   </div>
-                  <div className="col-span-1 text-center flex items-center justify-center">
-                    Friends
-                  </div>
-                </div>
-              </div>
-              <div className="row-span-1 p-3">
-                <div className="grid grid-cols-3 h-full">
-                  <div className="col-span-1 text-center flex items-center justify-center">
-                    {followers?.length ?? 0}
-                  </div>
-                  <div className="col-span-1 text-center flex items-center justify-center">
-                    {following?.length ?? 0}
-                  </div>
-                  <div className="col-span-1 text-center flex items-center justify-center">
-                    {currentUser?.friends.length ?? 0}
+                  <div className="flex justify-center items-center w-full h-full text-center ">
+                    <div className=" text-2xl col-span-1 text-center flex items-center justify-center">
+                      Friends
+                    </div>
+                    <div className=" text-white font-extrabold text-2xl mx-2 ">
+                      {currentUser?.friends?.length ?? 0}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="row-span-6 border-t-2">
+          <div className="row-span-6  border-neon-blue">
             <div className="grid grid-cols-2 h-screen overflow-hidden md:overflow-y-auto ">
-              <div className="col-span-2 md:col-span-1 border-r-2 ">
+              <div className="col-span-2 md:col-span-1  ">
                 <div className="flex flex-col h-full w-full items-center p-8">
                   <CustomTextField
                     className="w-full"
@@ -141,21 +144,21 @@ const Account = () => {
                       maxHeight: ' calc(60vh - 53px)',
                       overflowY: 'auto',
                     }}
-                    className=" w-full mt-4  border-2 border-neon-blue p-3 overflow-y-auto rounded-md"
+                    className=" w-full mt-4  border-x-2 shadow-inner border-t-gray-400 border-neon-blue p-3 overflow-y-auto "
                   >
                     {visibleUsers && visibleUsers.length > 0 ? (
                       visibleUsers.map((otherUser, index) => (
                         <>
                           <div
                             key={index}
-                            className="border-2 border-white w-full h-fit p-3 mt-4 grid grid-cols-2 gap-2 rounded-sm"
+                            className="bg-zinc-900 border-x-2 shadow-lg border-x-neon-blue   w-full h-fit p-3 mt-4 grid grid-cols-2 gap-2 rounded-sm"
                           >
                             {otherUser.name}
                             {following?.find(
                               user => user.id === otherUser.id
                             ) ? (
                               <button
-                                className="bg-gray-400 p-1 text-white  rounded-md hover:bg-gray-300"
+                                className="bg-gray-500 p-1 text-white  rounded-md hover:bg-gray-400"
                                 onClick={_ =>
                                   currentUser &&
                                   handleUnfollow(currentUser?.id, otherUser.id)
@@ -165,7 +168,7 @@ const Account = () => {
                               </button>
                             ) : (
                               <button
-                                className="bg-neon-blue p-1 text-white  rounded-md hover:bg-sky-300 "
+                                className="bg-neon-blue p-1 text-white  rounded-md hover:bg-sky-500 "
                                 onClick={_ =>
                                   currentUser &&
                                   handleFollow(currentUser?.id, otherUser.id)
