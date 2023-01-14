81839f9ce01acc9f0dbbbb474383abec0b76e309
RobPruzan <rapruzan@buffalo.edu>
2023-01-14 09:58:17 -0500

    Load more button (Api + frontend logic)

--- web/src/hooks/PostHooks/useGetPosts.ts
+++ web/src/hooks/PostHooks/useGetPosts.ts
@@ -1,21 +1,30 @@
 import WeCode, { PostContent } from '../../services/connections';
+import { useDispatch, useSelector } from 'react-redux';
 import { useQuery, useQueryClient } from 'react-query';
 
 import { PUBLIC_SPACE } from '../../components/MainPage/Options/JoinSpace/JoinSpace';
+import { PostAmountActions } from '../../redux/reducers/postAmount';
 import { RootState } from '../../redux/store';
-import { useSelector } from 'react-redux';
 
 export const useGetPosts = (fn?: (state: PostContent[]) => void) => {
   const queryClient = useQueryClient();
+  const dispatch = useDispatch();
+  const numberOfPosts = useSelector(
+    ({ postAmountState }: RootState) => postAmountState.amount
+  );
   const userId = useSelector(({ userState }: RootState) => userState.user?.id);
   const space_id = useSelector(
     ({ spaceState }: RootState) => spaceState.currentSpaceId
   );
   const { data, error, isLoading, isError, refetch } = useQuery(
-    ['space_posts', space_id, userId],
-    () => WeCode.getPosts(space_id ?? PUBLIC_SPACE),
+    ['space_posts', space_id, userId, numberOfPosts],
+    () => {
+      dispatch({ type: PostAmountActions.SetIsLoading });
+      return WeCode.getPosts(space_id ?? PUBLIC_SPACE, numberOfPosts);
+    },
     {
       onSuccess: data => {
+        dispatch({ type: PostAmountActions.SetIsNotLoading });
         fn && fn(data);
       },
       refetchOnWindowFocus: false,
