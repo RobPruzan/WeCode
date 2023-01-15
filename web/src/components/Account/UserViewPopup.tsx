8eb97a81e1a76dce7682ce59bb24a0508ce5eed6
RobPruzan <rapruzan@buffalo.edu>
2023-01-15 00:56:31 -0500

    Initial navigation of soaicl network through account page

--- web/src/components/Account/UserViewPopup.tsx
+++ web/src/components/Account/UserViewPopup.tsx
@@ -1,32 +1,60 @@
-import WeCode, { User } from '../../services/connections';
+import React, { Dispatch, SetStateAction, useState } from 'react';
+import WeCode, { User, UserMinimal } from '../../services/connections';
 
-import React from 'react';
+import { BsXCircle } from 'react-icons/bs';
+import { RootState } from '../../redux/store';
 import { UserConnectionTypes } from './UserConnections';
+import { useGetFollowers } from '../../hooks/UserHooks/useGetFollowers';
+import { useGetFollowing } from '../../hooks/UserHooks/useGetFollowing';
+import { useSelector } from 'react-redux';
 
 export type UserViewPopupProps = {
   users: User[] | null | undefined;
   userType: UserConnectionTypes;
   popupX: number;
   popupY: number;
+  setSelectedUserId: Dispatch<SetStateAction<UserMinimal | null | undefined>>;
 };
+
 const UserViewPopup = ({
   userType,
   users,
   popupX,
   popupY,
+  setSelectedUserId,
 }: UserViewPopupProps) => {
+  const currUserId = useSelector(
+    ({ userState }: RootState) => userState.user?.id
+  );
+  // const [selectedUserId, setSelectedUserId] = useState<
+  //   number | null | undefined
+  // >(currUserId);
+  // const getFollowersQuery = useGetFollowers(selectedUserId ?? 0);
+  // const getFollowingQuery = useGetFollowing(selectedUserId ?? 0);
+
   WeCode.getFollowers;
   return (
-    <div
-      className="bg-custom-dark-gray p-4 absolute rounded-lg shadow-md overflow-auto flex flex-col justify-center items-center z-40 h-56 border-2 border-white"
-      style={{ top: `${popupY}px`, left: `${popupX}px` }}
-    >
-      <p className="text-2xl font-bold">{userType}</p>
-      {users?.map(user => (
-        <div className="flex flex-row bg-zinc-900 justify-center items-center border-x-2 border-x-neon-blue w-full my-4 shadow-lg ">
-          <p className="text-xl font-bold">{user.name}</p>
-        </div>
-      ))}
+    <div style={{ top: `${popupY}px`, left: `${popupX}px` }}>
+      <div className="bg-custom-dark-gray p-4 absolute rounded-lg shadow-md overflow-auto flex flex-col justify-center items-center z-40 h-56 border-2 border-white ">
+        <p className="text-2xl font-bold">{userType}</p>
+
+        {users?.map(user => (
+          <button
+            onClick={e => {
+              setSelectedUserId({ id: user.id, label: user.name });
+              console.log(user.id);
+            }}
+            className="w-full"
+          >
+            <div className="flex flex-row px-4 bg-zinc-900 justify-center items-center border-x-2 border-x-neon-blue w-full my-4 shadow-lg hover:bg-slate-800 cursor-pointer">
+              <p className="text-xl font-bold">{user.name}</p>
+            </div>
+          </button>
+        ))}
+      </div>
+      {/* <button className="w-full z-50 rounded-lg bg-red-500 text-white text-xl p-2 hover:bg-red-400 shadow-lg">
+        Close
+      </button> */}
     </div>
   );
 };
