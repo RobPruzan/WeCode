0af6ad9c70609c2218ba99e4f9791fd9aad98c63
RobPruzan <97781863+RobPruzan@users.noreply.github.com>
2023-01-17 01:03:52 -0500

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
-import os
 
-DATABASES = os.environ.get("DATABASES", None)
+
+from .local_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
