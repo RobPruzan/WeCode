e5549400acd0e6f5667864b7e9fcb6fda3294042
RobPruzan <97781863+RobPruzan@users.noreply.github.com>
2023-01-17 01:04:17 -0500

    Create local_settings.py

---
+++ backend/backend/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "RSXn2YxmP9Naew4Uezz6",
+        "HOST": "containers-us-west-131.railway.app",
+        "PORT": "6952",
+    }
+}
